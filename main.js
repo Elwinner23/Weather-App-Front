f3fd4955e1d71287b0c9cbcc74594823072bd697
Elwin <elwinner23@yandex.ru>
2023-04-28 09:42:20 +0400

    Final version

--- main.js
+++ main.js
@@ -15,80 +15,20 @@ class API {
 
 const countryApi = new API("http://127.0.0.1:8000/");
 
-const createContinentElement = (item) => {
-  const continentElement = document.createElement("li");
-  const link = document.createElement("a");
-  link.href = "/country.html";
-  link.setAttribute("data-name", item);
-  link.id = "continent-link";
-  link.appendChild(document.createTextNode(item));
-  continentElement.appendChild(link);
 
-  return continentElement;
-};
 
-const updateContinentList = (data) => {
-  const continent = document.querySelector("ul");
-  data.map((continentItem) => {
-    continent.appendChild(createContinentElement(continentItem));
-  });
-};
-
-const getContinens = (data) => {
-  const continentsObj = {};
-  data.filter(({ continent }, index) => (continentsObj[continent] = index));
-  return Object.keys(continentsObj);
-};
-
-const updateCountryList = (data) => {
-  const continent = document.querySelector("ul");
-  data.map((continentItem) => {
-    continent.appendChild(createContinentElement(continentItem));
-  });
-};
-
-const getCountries = (data) =>
-  data.map(({ capital, top_cities }) => {
-    const countries = top_cities.map(({ name }) => name);
-    countries.push(capital);
-    return countries;
-  });
-
-const getCountryNameAndWeatherTemp = (data) => {
-  const nameAndTempArr = data[1].top_cities.map(({ name, weather }) => ({
-    name,
-    temp: weather[1].temperature.high,
-  }));
-
-  nameAndTempArr.push({
-    name: data[0].capital,
-    temp: data[0].weather[0].temperature.high,
-  });
-  return nameAndTempArr;
-};
 
 const createCountrBox = (data) => {
-  const countryContainer = document.querySelector(".country-container");
+  const countryContainer = document.querySelector(".city-country-container");
 
   data.map(
-    ({ name, temp }) =>
+    ({ city_name, dateweather }) =>
       (countryContainer.innerHTML += `<div class="country-box">
-  <h4 class="title">${name}</h4>
+  <h4 class="title">${city_name}</h4>
   <div class="country-box-body">
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="115"
-      height="115"
-      viewBox="0 0 256 256"
-    >
-      <path
-        fill="currentColor"
-        d="M156 72a76.2 76.2 0 0 0-20.26 2.73a55.63 55.63 0 0 0-9.41-11.54l9.51-13.57a8 8 0 1 0-13.11-9.18L113.22 54A55.9 55.9 0 0 0 88 48h-1.74l-2.89-16.29a8 8 0 1 0-15.75 2.77l2.88 16.34a56.1 56.1 0 0 0-23.27 14.85l-13.62-9.53a8 8 0 1 0-9.17 13.11L38 78.77A55.55 55.55 0 0 0 32 104v1.72l-16.29 2.88a8 8 0 0 0 1.38 15.88a8.24 8.24 0 0 0 1.39-.12l16.32-2.88a55.74 55.74 0 0 0 5.86 12.42A52 52 0 0 0 76 224h80a76 76 0 0 0 0-152ZM48 104a40 40 0 0 1 72.54-23.24a76.26 76.26 0 0 0-35.62 40a52.14 52.14 0 0 0-31 4.17A40 40 0 0 1 48 104Zm108 104H76a36 36 0 1 1 4.78-71.69c-.37 2.37-.63 4.79-.77 7.23a8 8 0 0 0 16 .92a58.91 58.91 0 0 1 1.88-11.81c0-.16.09-.32.12-.48A60.06 60.06 0 1 1 156 208Z"
-      />
-    </svg>
-
+    ${dateweather[0].status_icon}
     <div>
-      <p>${temp}º C</p>
+      <p>${dateweather[0].temperature}º C</p>
       <button class="details-btn">Details</button>
     </div>
   </div>
@@ -102,10 +42,17 @@ const getContinentNames = async () => {
 };
 
 const getCountryNames = async (continent) => {
-  let countryNames = await countryApi.getData("continentsAll/");
-  countryNames = countryNames.find(
-    (item) => item.continent_name == continent)?.country;
-   return countryNames;
+  let countryNames = await countryApi.getData(`continents/${continent}/`);
+ /* countryNames = countryNames.find(
+    (item) => item.continent_name == continent
+  )?.country;*/
+  return countryNames?.country;
+};
+
+const getCityNames = async (countri, continent) => {
+  const country = await getCountryNames(continent);
+  let cities = country.find((item) => item.conutry_name == countri)?.city;
+  return cities;
 };
 
 const main = async () => {
@@ -118,31 +65,46 @@ const main = async () => {
   //continentsName
   const continentsTitle = document.querySelectorAll(".country-box .title");
   const continentsLink = document.querySelectorAll(".continent-container a");
+  const citiesContinent = document.querySelectorAll(".country-container");
   continentsTitle.forEach((item, index) => {
     item.innerText = continentNames[index];
     continentsLink[index].setAttribute(
       "data-continent-name",
       continentNames[index]
     );
-    continentsLink[index].addEventListener("click", (event) => {
-      localStorage.setItem("continent", event.target.innerText);
+    continentsLink[index].addEventListener("click", () => {
+      localStorage.setItem(
+        "continent",
+        continentsLink[index].getAttribute("data-continent-name")
+      );
     });
   });
   const countries = await getCountryNames(localStorage.getItem("continent"));
   countries.map(({ conutry_name, flag }) => {
     const cityDiv = document.querySelector(".city-container");
-    cityDiv.innerHTML += `<a href="country.html">
-      <div class="city-box">
+    cityDiv &&
+      (cityDiv.innerHTML += `<a data-city-name=${conutry_name} href="country.html">
+      <div class="city-box" >
         <span class="flag">${flag}</span>
         <h4 class="title">${conutry_name}</h4>
       </div>
-    </a>`;
-    /*  console.log(countries[index].conutry_name);
-  console.log(countries[index].flag);
-*/
+    </a>`);
   });
-  /*createCountrBox(countryNameAndWeatherTemp);
-  updateContinentList(continets);*/
+  const cityLink = document.querySelectorAll(".city-container a");
+
+  cityLink.forEach((item) =>
+    item.addEventListener("click", () => {
+      localStorage.setItem("city", item.getAttribute("data-city-name"));
+    })
+  );
+  const cityNames = await getCityNames(
+    localStorage.getItem("city"),
+    localStorage.getItem("continent")
+  );
+  console.log(cityNames);
+
+  createCountrBox(cityNames);
+  // updateContinentList(continets);
 };
 
 main();
